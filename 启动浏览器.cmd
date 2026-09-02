e498f7047dfcd48c18a1a06e2c46ba13c219bae1
beifangzhishi-ops <beifangzhishi@gmail.com>
2026-09-02 17:55:32 +0800

    Remove obsolete CDP browser launcher

--- 启动浏览器.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\start-browser.ps1"
-set "BROWSER_EXIT_CODE=%ERRORLEVEL%"
-if not "%BROWSER_EXIT_CODE%"=="0" pause
-exit /b %BROWSER_EXIT_CODE%
